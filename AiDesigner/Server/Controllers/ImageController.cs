b44907bb7010e452d8032a828f633406f60e3595
NodeNestor <126032480+Nodenester@users.noreply.github.com>
2023-10-11 20:37:22 +0200

    fixed so the chart show the whole span not just the ones with data   anhd addee suport for pontentially chaching images

---
+++ AiDesigner/Server/Controllers/ImageController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AiDesigner.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ImagesController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult UploadImage([FromBody] byte[] imageData)
+        {
+            var fileName = $"images/{Guid.NewGuid()}.png";
+            var filePath = Path.Combine("wwwroot", fileName);
+            System.IO.File.WriteAllBytes(filePath, imageData);
+            return Ok(fileName);
+        }
+    }
+
+}
