fd547ef1b09ec99f7154cee141a20b12d53e27d1
ludde <ludvig.kalmarit@gmail.com>
2023-10-11 20:37:22 +0200

    fixed so the chart show the whole span not just the ones with data   anhd addee suport for pontentially chaching images

--- AiDesigner/Server/Controllers/ChatController.cs
+++ AiDesigner/Server/Controllers/ChatController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Text.Json;
 using System.Threading.Tasks;
 using AiDesigner.Server.Data;
