a2d41dc64be1d941d1faee5a9d3ee0552e87a7f8
ludde <ludvig.kalmarit@gmail.com>
2023-11-17 04:46:47 +0100

    added a bunch of ai blocks

---
+++ AiDesigner/Shared/Blocks/AiModels/LlmBlocks.cs
@@ -0,0 +1,71 @@
+﻿using NodeBaseApi.Version2;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Type = NodeBaseApi.Version2.Type;
+
+namespace NodeExacuteApi.Data.Blocks.AiModels
+{
+    public class Llama27b : Block
+    {
+        public Llama27b()
+        {
+            Id = Guid.NewGuid();
+            Name = "Llama2 7b";
+            Description = "Llama 2-7b API";
+            Inputs = new List<Input>
+            {
+                new Input { Name = "Query", Type = Type.String, Description = "The input query for the Llama 2-7b API" }
+            };
+            Outputs = new List<Output>
+            {
+                new Output { Name = "ApiResponse", Type = Type.String, Description = "The API response from Llama 2-7b" }
+            };
+        }
+
+        public override List<object> Execute(List<object> inputs, ProgramStructure programStructure) { return null; }
+    }
+
+    public class Llama213b : Block
+    {
+        public Llama213b()
+        {
+            Id = Guid.NewGuid();
+            Name = "Llama2 13b";
+            Description = "Llama 2-13b API";
+            Inputs = new List<Input>
+        {
+            new Input { Name = "Query", Type = Type.String, Description = "The input query for the Llama 2-13b API" }
+        };
+            Outputs = new List<Output>
+        {
+            new Output { Name = "ApiResponse", Type = Type.String, Description = "The API response from Llama 2-13b" }
+        };
+        }
+
+        public override List<object> Execute(List<object> inputs, ProgramStructure programStructure) { return null; }
+    }
+
+    public class Llama270b : Block
+    {
+        public Llama270b()
+        {
+            Id = Guid.NewGuid();
+            Name = "Llama2 70b";
+            Description = "Llama 2-70b API";
+            Inputs = new List<Input>
+        {
+            new Input { Name = "Query", Type = Type.String, Description = "The input query for the Llama 2-70b API" }
+        };
+            Outputs = new List<Output>
+        {
+            new Output { Name = "ApiResponse", Type = Type.String, Description = "The API response from Llama 2-70b" }
+        };
+        }
+
+        public override List<object> Execute(List<object> inputs, ProgramStructure programStructure) { return null; }
+    }
+}
