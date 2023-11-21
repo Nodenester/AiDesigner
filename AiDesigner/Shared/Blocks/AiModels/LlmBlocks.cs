2d1957a1020a7e28b114e9653bb34502bd086c38
NodeNestor <126032480+Nodenester@users.noreply.github.com>
2023-11-21 04:22:58 +0100

    dont remember some bugs probably

--- AiDesigner/Shared/Blocks/AiModels/LlmBlocks.cs
+++ AiDesigner/Shared/Blocks/AiModels/LlmBlocks.cs
@@ -18,11 +18,12 @@ namespace NodeExacuteApi.Data.Blocks.AiModels
             Description = "Llama 2-7b API";
             Inputs = new List<Input>
             {
-                new Input { Name = "Query", Type = Type.String, Description = "The input query for the Llama 2-13b API", IsRequired = true },
-                new Input { Name = "MaxNewTokens", Type = Type.Number, Description = "Maximum new tokens to be generated", IsRequired = true},
-                new Input { Name = "TopP", Type = Type.Number, Description = "Top P value for controlling randomness", IsRequired = true },
-                new Input { Name = "Temperature", Type = Type.Number, Description = "Temperature value for controlling creativity", IsRequired = true },
-                new Input { Name = "StopWords", Type = Type.String, IsList = true, Description = "List of words where the AI should stop generating text", IsRequired = false }
+                new Input { Name = "Query", Type = Type.String, Description = "The input query for the Llama 2-7b API", IsRequired = true },
+                new Input { Name = "MaxNewTokens", Type = Type.Number, Description = "Maximum new tokens to be generated (Max tokens 4096)", IsRequired = false},
+                new Input { Name = "TopP", Type = Type.Number, Description = "Top P value for controlling randomness", IsRequired = false },
+                new Input { Name = "Temperature", Type = Type.Number, Description = "Temperature value for controlling creativity", IsRequired = false },
+                new Input { Name = "StopWords", Type = Type.String, IsList = true, Description = "List of words where the AI should stop generating text", IsRequired = false },
+                new Input { Name = "ReturnFullText", Type = Type.Boolean, Description = "Whether to include the input prompt in the API response", IsRequired = false }
             };
             Outputs = new List<Output>
             {
@@ -43,10 +44,11 @@ namespace NodeExacuteApi.Data.Blocks.AiModels
             Inputs = new List<Input>
             {
                 new Input { Name = "Query", Type = Type.String, Description = "The input query for the Llama 2-13b API", IsRequired = true },
-                new Input { Name = "MaxNewTokens", Type = Type.Number, Description = "Maximum new tokens to be generated", IsRequired = true},
-                new Input { Name = "TopP", Type = Type.Number, Description = "Top P value for controlling randomness", IsRequired = true },
-                new Input { Name = "Temperature", Type = Type.Number, Description = "Temperature value for controlling creativity", IsRequired = true },
-                new Input { Name = "StopWords", Type = Type.String, IsList = true, Description = "List of words where the AI should stop generating text", IsRequired = false }
+                new Input { Name = "MaxNewTokens", Type = Type.Number, Description = "Maximum new tokens to be generated (Max tokens 4096)", IsRequired = false},
+                new Input { Name = "TopP", Type = Type.Number, Description = "Top P value for controlling randomness", IsRequired = false },
+                new Input { Name = "Temperature", Type = Type.Number, Description = "Temperature value for controlling creativity", IsRequired = false },
+                new Input { Name = "StopWords", Type = Type.String, IsList = true, Description = "List of words where the AI should stop generating text", IsRequired = false },
+                new Input { Name = "ReturnFullText", Type = Type.Boolean, Description = "Whether to include the input prompt in the API response", IsRequired = false }
             };
             Outputs = new List<Output>
             {
@@ -66,16 +68,37 @@ namespace NodeExacuteApi.Data.Blocks.AiModels
             Description = "Llama 2-70b API";
             Inputs = new List<Input>
             {
-                new Input { Name = "Query", Type = Type.String, Description = "The input query for the Llama 2-13b API", IsRequired = true },
-                new Input { Name = "MaxNewTokens", Type = Type.Number, Description = "Maximum new tokens to be generated", IsRequired = true},
-                new Input { Name = "TopP", Type = Type.Number, Description = "Top P value for controlling randomness", IsRequired = true },
-                new Input { Name = "Temperature", Type = Type.Number, Description = "Temperature value for controlling creativity", IsRequired = true },
-                new Input { Name = "StopWords", Type = Type.String, IsList = true, Description = "List of words where the AI should stop generating text", IsRequired = false }
+                new Input { Name = "Query", Type = Type.String, Description = "The input query for the Llama 2-70b API", IsRequired = true },
+                new Input { Name = "MaxNewTokens", Type = Type.Number, Description = "Maximum new tokens to be generated (Max tokens 4096)", IsRequired = false},
+                new Input { Name = "TopP", Type = Type.Number, Description = "Top P value for controlling randomness", IsRequired = false },
+                new Input { Name = "Temperature", Type = Type.Number, Description = "Temperature value for controlling creativity", IsRequired = false },
+                new Input { Name = "StopWords", Type = Type.String, IsList = true, Description = "List of words where the AI should stop generating text", IsRequired = false },
+                new Input { Name = "ReturnFullText", Type = Type.Boolean, Description = "Whether to include the input prompt in the API response", IsRequired = false }
             };
             Outputs = new List<Output>
             {
                 new Output { Name = "ApiResponse", Type = Type.String, Description = "The API response from Llama 2-70b" }
             };
+        }
+
+        public override List<object> Execute(List<object> inputs, ProgramStructure programStructure) { return null; }
+    }
+
+    public class CodeLlama34b : Block
+    {
+        public CodeLlama34b()
+        {
+            Id = Guid.NewGuid();
+            Name = "CodeLlama 34b";
+            Description = "CodeLlama 34b API Integration";
+            Inputs = new List<Input>
+        {
+            new Input { Name = "Query", Type = Type.String, Description = "The input query for the CodeLlama 34b API", IsRequired = true }
+        };
+            Outputs = new List<Output>
+        {
+            new Output { Name = "ApiResponse", Type = Type.String, Description = "The API response from CodeLlama 34b" }
+        };
         }
 
         public override List<object> Execute(List<object> inputs, ProgramStructure programStructure) { return null; }
