ab4b19aa0967d6c86ac525fa59efcce491f93191
ludde <ludvig.kalmarit@gmail.com>
2024-01-21 14:45:54 +0100

    right .net 8:))

--- AiDesigner/Client/Program.cs
+++ AiDesigner/Client/Program.cs
@@ -5,8 +5,6 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddHttpClient("AiDesigner.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
