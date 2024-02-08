04fb708c92ceb922ea835551d9530e8f35703b05
NodeNestor <126032480+Nodenester@users.noreply.github.com>
2024-02-08 21:14:26 +0100

    yay added beta tester

--- AiDesigner/Server/Data/Migrations/20231002001624_RolesAdded.cs
+++ AiDesigner/Server/Data/Migrations/20231002001624_RolesAdded.cs
@@ -14,6 +14,10 @@ namespace AiDesigner.Server.Data.Migrations
                 table: "AspNetRoles",
                 columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
                 values: new object[] { "8cf4210b-6bfc-4c34-82b2-d3c1756d8980", null, "Administrator", "ADMINISTRATOR" });
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "cc98cd23-a530-46fc-935a-d2cb744672dd", null, "BetaTester", "BetaTester" });
         }
 
         /// <inheritdoc />
@@ -23,6 +27,10 @@ namespace AiDesigner.Server.Data.Migrations
                 table: "AspNetRoles",
                 keyColumn: "Id",
                 keyValue: "8cf4210b-6bfc-4c34-82b2-d3c1756d8980");
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "cc98cd23-a530-46fc-935a-d2cb744672dd");
         }
     }
 }
