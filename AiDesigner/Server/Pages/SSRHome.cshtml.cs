3d7a337838b0506b1577072b3e6d0d16bee2142b
NodeNestor <126032480+Nodenester@users.noreply.github.com>
2024-02-23 00:55:32 +0100

    test seo thing:= exetra

---
+++ AiDesigner/Server/Pages/SSRHome.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AiDesigner.Server.Pages
+{
+    public class PrerenderTestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
