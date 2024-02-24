ede14665cac99a7916db43926099151c81dcc153
NodeNestor <126032480+Nodenester@users.noreply.github.com>
2024-02-24 03:42:36 +0100

    will see hmm tommorow

--- AiDesigner/Client/Program.cs
+++ AiDesigner/Client/Program.cs
@@ -7,7 +7,11 @@ using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress),
+    Timeout = TimeSpan.FromMinutes(10)
+});
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
